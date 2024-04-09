91495695a89ef1b7a636d98b09cee1d1ff8e59ae
Lilian G <97319339+gimenezt@users.noreply.github.com>
2024-04-09 19:33:03 -0300

    Upload de exercício 2

---
+++ exercicio02.py
@@ -0,0 +1,19 @@
+def insere_ordenada(lista, x):
+    idx = 0
+
+    # verifica a qual numero x eh maior para coloca-lo em sequencia
+    for item in lista:
+        if x > item:
+            idx = lista.index(item) + 1
+
+    nova_lista = lista[:idx] + [x] + lista[idx:]
+
+    return nova_lista
+
+# input de variaveis
+
+lista_ordenada = str(input("Digite os numeros em ordem crescente, separados por espaco: ")).split()
+lista_ordenada = [int(i) for i in lista_ordenada]
+x = int(input("Digite o numero que quer inserir nessa lista: "))
+
+print(insere_ordenada(lista_ordenada, x))
